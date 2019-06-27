27801629ca91bed9193b94f1ccb7c78e7307646a
Ian <iansmacbook@Ians-MacBook-Pro.local>
2019-06-27 12:44:48 +0800

    Updates

---
+++ precache-manifest.313c5215a70fbfe3d414ee064bdf9ec4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bc07c13adffe9e8bcea70471ce156a6c",
+    "url": "/testapp/index.html"
+  },
+  {
+    "revision": "b65b1b86a580e2303398",
+    "url": "/testapp/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "2e0129063922e3bb197a",
+    "url": "/testapp/static/js/2.b41502e9.chunk.js"
+  },
+  {
+    "revision": "b65b1b86a580e2303398",
+    "url": "/testapp/static/js/main.bba0afe9.chunk.js"
+  },
+  {
+    "revision": "18ef34af1b850974dbed",
+    "url": "/testapp/static/js/runtime~main.4c82991b.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/testapp/static/media/logo.5d5d9eef.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.054774adbe886ee6e3c29227ef1745b5.js"
+  "/testapp/precache-manifest.313c5215a70fbfe3d414ee064bdf9ec4.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/testapp/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
